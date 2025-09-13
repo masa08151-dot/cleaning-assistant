d402996ec11de0c6dc793cf612cc11ac524193f6
masa08151-dot <masa08151@gmail.com>
2025-09-13 16:52:58 +0900

    Delete src/app directory

--- src/app/src/lib/src/lib/src/lib/src/lib/pdf.ts
+++
@@ -1,9 +0,0 @@
-import puppeteer from 'puppeteer';
-export async function htmlToPdfBuffer(html: string) {
-  const browser = await puppeteer.launch({ args: ['--no-sandbox','--disable-setuid-sandbox'] });
-  const page = await browser.newPage();
-  await page.setContent(html, { waitUntil: 'networkidle0' });
-  const pdf = await page.pdf({ format: 'A4', printBackground: true, margin: { top: '14mm', bottom: '14mm', left: '12mm', right: '12mm' } });
-  await browser.close();
-  return pdf;
-}
